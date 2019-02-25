0da563a4196716eef2b326edf933c8868daf0506
Sigma9805 <47579905+Sigma9805@users.noreply.github.com>
2019-02-24 22:06:03 -0600

    Add files via upload

--- Practica 1/Script.js
+++ Practica 1/Script.js
@@ -1,3 +1,23 @@
-function clickear(argument) {
-	var x = 0;
+function retornarFecha()
+{
+  var fecha
+  fecha=new Date();
+  var cadena=fecha.getDate()+'/'+(fecha.getMonth()+1)+'/'+fecha.getYear();
+  alert(fecha);
+}
+
+function retornar()
+{
+alert("No hay nada que buscar");
+}
+
+
+function controlMenu()
+{
+ document.getElementById("open-hide").classList.toggle('show');
+}
+
+window.onload = function() 
+{
+  document.getElementById("open-hide").addEventListener( 'click' , changeClass ); 
 }
